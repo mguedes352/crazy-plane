cacfcaadcb6785e73543412ba70e7e533d08429a
Matheus Guedes <79527238+mguedes352@users.noreply.github.com>
2021-10-25 22:01:16 -0300

    crazy-plane-v1

--- game-aviao/game-aviao-v1.py
+++ game-aviao/game-aviao-v1.py
@@ -1,5 +1,4 @@
 import pygame
-from pygame.draw import ellipse
 from pygame.locals import *
 from sys import exit
 
@@ -9,27 +8,23 @@ largura = 900
 altura = 500
 screen = pygame.display.set_mode((largura, altura), 0, 32)
 
-azul_claro = ((173, 216, 230))
-cor_diferente = ((48, 23, 45))
+ceu_azul_claro = ((173, 216, 230))
+ceu_roxo = ((48, 23, 45))
 
 
-predios1_image_filename = 'background.png'
-predios1 = pygame.image.load(predios1_image_filename).convert_alpha()
-predios2_image_filename = 'background.png'
-predios2 = pygame.image.load(predios2_image_filename).convert_alpha()
-
-aviao_filename = "aviao.png"
-aviao = pygame.image.load(aviao_filename).convert_alpha()
+predios1_surface = pygame.image.load('background/background.png').convert_alpha()
+predios2_surface = pygame.image.load('background/background.png').convert_alpha()
+aviao_surface = pygame.image.load("avioes/aviao-john.png").convert_alpha()
 
 
-FPS = 60
+FPS = 120
 clock = pygame.time.Clock()
 
-pygame.display.set_caption('Fase 1')
+pygame.display.set_caption('Game')
 
 
-x_aviao = 100
-y_aviao = 100
+x_aviao = 30
+y_aviao = 120
 
 x_predios1 = 0
 y_predios1 = 0
@@ -37,26 +32,37 @@ y_predios1 = 0
 x_predios2 = 1800
 y_predios2 = 0
 
+#Variaveis de jogo
+gravidade = 0.1
+movimento_aviao = 0
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
-    x_predios1 -= 5
+        
+        if event.type == KEYDOWN:
+            if event.key == K_SPACE:
+                movimento_aviao = 0
+                movimento_aviao -= 5
+
+    x_predios1 -= 3
     if x_predios1 <= -1800:
         x_predios1 = 1800
     
-    x_predios2 -= 5
+    x_predios2 -= 3
     if x_predios2 <= -1800:
         x_predios2 = 1800
 
+    movimento_aviao += gravidade
+    y_aviao += movimento_aviao 
+
+    background = screen.fill(ceu_roxo)
     
-    background = screen.fill(cor_diferente)
-
-    screen.blit(predios1,(x_predios1, y_predios1))
-    screen.blit(predios2,(x_predios2, y_predios2))
-    screen.blit(aviao, (x_aviao, y_aviao))
-
+    screen.blit(predios1_surface,(x_predios1, y_predios1))
+    screen.blit(predios2_surface,(x_predios2, y_predios2))
+    screen.blit(aviao_surface, (x_aviao, y_aviao))
 
 
     pygame.display.update()
